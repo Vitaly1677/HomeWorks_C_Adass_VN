69a0e59168f92ca83233886828497ef7ca32d20f
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-21 18:21:13 +0300

    Добавил решение Task04

--- Lesson_01_Task04/Program.cs
+++ Lesson_01_Task04/Program.cs
@@ -1,7 +1,28 @@
-﻿Задача 4: Напишите программу, 
-которая принимает на вход три числа и 
-выдаёт максимальное из этих чисел.
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
 
-2, 3, 7 -> 7
-44 5 78 -> 78
-22 3 9 -> 22
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите 1-е число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+
+if (secondNumber > max)
+{
+    max = secondNumber;
+}
+
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+
+Console.WriteLine($"Максимальное число {max}");
