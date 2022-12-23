561f6c4b581ebf3c0a2ab7be689ee8d8ad5538a8
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-23 14:40:22 +0300

    Добавил решение Task15

--- Lesson_02_Task15/Program.cs
+++ Lesson_02_Task15/Program.cs
@@ -4,3 +4,16 @@
 // 6 -> да 
 // 7 -> да 
 // 1 -> нет
+
+Console.Write("Введите номер дня недели, для определения выходного дня: ");
+int day = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{day} -> {Weekend(day)}");
+string Weekend(int nom)
+{
+    if (nom <= 7 && nom >= 1)
+    {
+        if (nom == 6 || nom == 7) return "Да";
+        else return "Нет";
+    }
+    else return "Нет такого дня недели, он только в Ваших мечтах.";
+}
