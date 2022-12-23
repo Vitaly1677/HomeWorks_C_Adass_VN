7f18ba1a096cdfb90cde5aa4562e8a7e4ffdb313
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-23 17:55:22 +0300

    Исправил Task15 в соотв. с замечаниями преподавателя: добавил проверку на вводе данных, метод сделал bool.

--- Lesson_02_Task15/Program.cs
+++ Lesson_02_Task15/Program.cs
@@ -4,16 +4,17 @@
 // 6 -> да 
 // 7 -> да 
 // 1 -> нет
-
+begin:
 Console.Write("Введите номер дня недели, для определения выходного дня: ");
 int day = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"{day} -> {Weekend(day)}");
-string Weekend(int nom)
+if (day > 7 || day < 1)
 {
-    if (nom <= 7 && nom >= 1)
-    {
-        if (nom == 6 || nom == 7) return "Да";
-        else return "Нет";
-    }
-    else return "Нет такого дня недели, он только в Ваших мечтах.";
+    Console.WriteLine("Вы ввели не верный день недели, попробуйте еще раз.");
+    goto begin;
+}
+Console.WriteLine(Weekend(day) ? "Да" : "Нет");
+
+bool Weekend(int nom)
+{
+    return nom == 6 || nom == 7;
 }
