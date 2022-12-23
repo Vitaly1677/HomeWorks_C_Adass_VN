6c2947961a675fd626dcacf7743678e79e1a1195
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-23 14:00:57 +0300

    Добавил решение Task14

--- Lesson_02_Task13/Program.cs
+++ Lesson_02_Task13/Program.cs
@@ -4,3 +4,28 @@
 // 645 -> 5 
 // 78 -> третьей цифры нет 
 // 32679 -> 6
+
+Console.Write("Введите число, для отображения 3-ей цифры: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(ThirdNumberWrite(number) != -1 ? $"{number} -> {ThirdNumberWrite(number)}" : "А третьей цифры то нет... :)");
+int ThirdNumberWrite(int num1)
+{
+    if (num1 > 999)
+    {
+        while (num1 > 999)
+        {
+            num1 = num1 / 10;
+        }
+        return num1 % 10;
+    }
+    else if (num1 > 99)
+    {
+        return num1 % 10;
+    }
+    else
+    {
+        return -1;
+    }
+
+
+}
