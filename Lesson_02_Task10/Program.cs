d4b5a85fbc11aa8534e4167c7ffe51a53aac8da3
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-23 12:10:07 +0300

    Добавил решение Task10

--- Lesson_02_Task10/Program.cs
+++ Lesson_02_Task10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая 
+// принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5 
+// 782 -> 8 
+// 918 -> 1
+
+Console.Write("Ведите число: ");
+int number = Convert.ToInt32(Console.Read());
+//int number = 782;
+Console.WriteLine($"{number}  ->  {SecondNumberWrite(number)}");
+int SecondNumberWrite(int num)
+{
+    return num / 10 - num / 100 * 10;
+}
