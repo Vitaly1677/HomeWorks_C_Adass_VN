b16b1e1317ce356e485d62de1fb7c95e577d99c0
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-26 12:27:03 +0300

    Добавил решение Task19

--- Lesson_03_Task19/Program.cs
+++ Lesson_03_Task19/Program.cs
@@ -8,3 +8,21 @@
 // 12821 -> да
 // 23432 -> да
 
+Begin:
+Console.Write("Введите 5-и знач. число, для проверки, является ли оно палиндромом: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number < 10000 || number > 99999 ) 
+{
+    Console.WriteLine("Число не 5-и значное, повторите ввод.");
+    goto Begin;
+}
+Console.Write ($"{number} -> ");
+Console.WriteLine(PolyTest(number) ?  "да" : "нет");
+
+bool PolyTest(int num)
+{
+int numberPoly1 = num / 1000;
+int numberPoly2 = num % 10 * 10 + number /10 % 10;
+return numberPoly1 == numberPoly2;
+}
