81aaa7ff26eb66a038c4e816485f15d3d0893986
Git GeekPythonUser <geekpython@yandex.ru>
2022-12-28 17:31:37 +0300

    Добавил решение Task23

--- Lesson_03_Task23/Program.cs
+++ Lesson_03_Task23/Program.cs
@@ -7,3 +7,20 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+Console.Write("Введите число (N) для выдачи таблицы кубов чисел от 1 до N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) TableSquare(number);
+else System.Console.WriteLine("Введено не натуральное число.");
+
+void TableSquare(int num)
+{
+    int count = 1;
+    System.Console.WriteLine($"{number} ->");
+    while (count <= number)
+    {
+        Console.WriteLine($"{count} | {Math.Pow(count, 3)}");
+        count++;
+    }
+}
