a36e8b24185c53ab45454ea6c5467983b25168e9
Git GeekPythonUser <geekpython@yandex.ru>
2023-01-19 17:32:02 +0300

    Добавил решение задачи Task27

--- Lesson_04_Task27/Program.cs
+++ Lesson_04_Task27/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая 
+// принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int countNumbers = CountNumbers(number);
+Console.WriteLine($"Количество чисел в {number} -> {countNumbers}");
+int CountNumbers(int num)
+{
+    if (num == 0) return 1;
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
