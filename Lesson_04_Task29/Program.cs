1560dfe8ee9205d6f87fd51d28117849d6680463
Git GeekPythonUser <geekpython@yandex.ru>
2023-01-19 17:48:55 +0300

    Добавил изменения в метод PrintArray  задачи Task29

--- Lesson_04_Task29/Program.cs
+++ Lesson_04_Task29/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и 
+// выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i != 0) Console.Write(", ");
+        Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
