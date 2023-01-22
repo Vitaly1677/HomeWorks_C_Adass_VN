49221577f5a6ac2ca96fcd6abb5537e3414fed4b
Vitaly Adass <geekpython16@yandex.ru>
2023-01-22 23:56:17 +0300

    Подправил внешний вид

--- Lesson_01_Task02/Program.cs
+++ Lesson_01_Task02/Program.cs
@@ -4,7 +4,6 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
 Console.Write("Введите 1-е число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-е число: ");
