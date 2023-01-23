27263759cf006284f42a375a47d6fc1cb1d7ff92
Vitaly Adass <geekpython16@yandex.ru>
2023-01-23 18:25:07 +0300

    Добавил решение задачи Task36

--- Lesson_05_Task36/Program.cs
+++ Lesson_05_Task36/Program.cs
@@ -3,3 +3,41 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] array = CreateArrayRndInt(4, 1, 50);
+PrintArray(array);
+
+int sumNumOddPos = SumNumOddPosArray(array);
+
+Console.WriteLine($"Сумма чисел на нечетных поз. в массиве = {sumNumOddPos}");
+
+int SumNumOddPosArray(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if ((i + 1) % 2 == 0) sum += arr[i];
+    }
+    return sum;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
