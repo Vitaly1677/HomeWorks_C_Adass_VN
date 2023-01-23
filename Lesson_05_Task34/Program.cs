33ab8bbb242f216c08cd0cefc9dde2c11011ba9f
Vitaly Adass <geekpython16@yandex.ru>
2023-01-23 18:14:08 +0300

    Добавил решение задачи Task34

--- Lesson_05_Task34/Program.cs
+++ Lesson_05_Task34/Program.cs
@@ -3,3 +3,45 @@
 
 // [345, 897, 568, 234] -> 2
 
+
+int[] array = CreateArrayRndInt(5, 100, 999);
+PrintArray(array);
+
+int countEven = CountEvenNum(array);
+
+Console.WriteLine($"Колличество четных чисел в массиве = {countEven}");
+
+int CountEvenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i]);
+    }
+    Console.WriteLine("]");
+}
+
+
+
