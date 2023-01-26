9f8b4436e1489a24095ffb8a2fd958187f1babf7
Vitaly Adass <geekpython16@yandex.ru>
2023-01-26 15:04:30 +0300

    Добавил решение задачи  Task41

--- Lesson_06_Task41/Program.cs
+++ Lesson_06_Task41/Program.cs
@@ -3,10 +3,44 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
+int count = ReadIntNumder("Bведите колличество вводимых чисел: ");
+int[] array = new int[count];
+array = ReadNumberToArray(count);
+PrintArray(array);
+int countPositiveNumbers = CountPositiveNumbersArray(array);
+Console.WriteLine(" -> " + countPositiveNumbers);
 
-
-int ReadIntNumder(int index)
+int[] ReadNumberToArray(int count)
 {
-    Console.Write($"Введите число номер {index}: ");
+    int[] arr = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        arr[i] = ReadIntNumder("Введите число массива N " + (i + 1) + " из " + count + ": ");
+    }
+    return arr;
+}
+
+int ReadIntNumder(string ParamName)
+{
+    Console.Write(ParamName);
     return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountPositiveNumbersArray(int[] arr)
+{
+    int cou = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]> 0) cou++;
+    }
+    return cou;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
 }
