03324e3da0ccbf15277a8043bd6d4ec26d8ac395
Vitaly Adass <geekpython16@yandex.ru>
2023-01-26 17:57:15 +0300

    Добавил решение задачи Task43

--- Lesson_06_Task43/Program.cs
+++ Lesson_06_Task43/Program.cs
@@ -2,4 +2,55 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// 
+// Упрощаем уровнение:
+// y = k1 * x + b1, y = k2 * x + b2;
+// k1 * x + b1 = k2 * x + b2
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
 
+
+
+double b1 = ReadDoubleNumder("Bведите b1: ");
+double k1 = ReadDoubleNumder("Bведите k1: ");
+double b2 = ReadDoubleNumder("Bведите b2: ");
+double k2 = ReadDoubleNumder("Bведите k2: ");
+
+if (ChkCoincidence(b1, k1, b2, k2))
+{
+    Console.WriteLine("Прямые совпадают");
+}
+else if (ChkParralel(k1, k2))
+{
+    Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({CalcX(b1, k1, b2, k2)}; {CalcY(b1, k1, b2, k2)})");
+}
+
+
+bool ChkCoincidence(double b1, double k1, double b2, double k2)
+{
+    return ((k1 == k2) && (b1 == b2));
+}
+bool ChkParralel(double k1, double k2)
+{
+    return (k1 == k2);
+}
+
+double CalcX(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+
+double CalcY(double b1, double k1, double b2, double k2)
+{
+    return (k1 * CalcX(b1, k1, b2, k2) + b1);
+}
+
+double ReadDoubleNumder(string ParamName)
+{
+    Console.Write(ParamName);
+    return Convert.ToDouble(Console.ReadLine());
+}
