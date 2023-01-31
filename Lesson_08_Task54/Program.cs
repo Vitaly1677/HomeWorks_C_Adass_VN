2f3341e42d765f8aa168a505cabad7d46f3d2f3d
Vitaly Adass <geekpython16@yandex.ru>
2023-02-01 02:11:01 +0300

    Добавил задание

--- Lesson_08_Task54/Program.cs
+++ Lesson_08_Task54/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
