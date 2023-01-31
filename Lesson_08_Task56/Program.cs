8719bd69f39b84df177199f594efa4f556afa09f
Vitaly Adass <geekpython16@yandex.ru>
2023-02-01 02:15:09 +0300

    Добавил задание

--- Lesson_08_Task56/Program.cs
+++ Lesson_08_Task56/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите
+// программу, которая будет находить строку с наименьшей суммой элементов.
