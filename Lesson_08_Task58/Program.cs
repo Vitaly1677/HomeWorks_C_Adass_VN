19c8d3fb1efaf0494faf66279812b6e0a165b511
Vitaly Adass <geekpython16@yandex.ru>
2023-02-01 10:07:27 +0300

    Добавил задание

--- Lesson_08_Task58/Program.cs
+++ Lesson_08_Task58/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет
+// находить произведение двух матриц.
