3c437a26ccc8c62ede5902161af682e630e77021
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 12:49:59 +0300

    Добавил решение

--- Lesson_09_Task63/Program.cs
+++ Lesson_09_Task63/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите первое число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberN > 0)
+{
+    Console.Write("N = " + numberN + " -> ");
+    NaturalNumbersRange(numberN);
+    Console.WriteLine("");
+}
+else Console.WriteLine("Число должно быть больше 0");
+
+void NaturalNumbersRange(int num)
+{
+    if (num == 1) Console.Write("1");
+    else
+    {
+        NaturalNumbersRange(num - 1);
+        Console.Write(", ");
+        Console.Write($"{num}");
+    }
+}
