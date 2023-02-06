31cb857f586af96ef86db559cd71ceae5106c57f
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 13:55:17 +0300

    Добавил решение

--- Lesson_09_Task63/Program.cs
+++ Lesson_09_Task63/Program.cs
@@ -1,16 +1,16 @@
-﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
-// все натуральные числа в промежутке от 1 до N.
-// N = 5 -> "1, 2, 3, 4, 5"
-// N = 6 -> "1, 2, 3, 4, 5, 6"
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите первое число N: ");
+Console.Write("Введите число N: ");
 int numberN = Convert.ToInt32(Console.ReadLine());
 
 if (numberN > 0)
 {
-    Console.Write("N = " + numberN + " -> ");
+    Console.Write("N = " + numberN + " -> \"");
     NaturalNumbersRange(numberN);
-    Console.WriteLine("");
+    Console.WriteLine("\"");
 }
 else Console.WriteLine("Число должно быть больше 0");
 
@@ -19,8 +19,8 @@ void NaturalNumbersRange(int num)
     if (num == 1) Console.Write("1");
     else
     {
-        NaturalNumbersRange(num - 1);
-        Console.Write(", ");
         Console.Write($"{num}");
+        Console.Write(", ");        
+        NaturalNumbersRange(num - 1);        
     }
 }
