72f9c24385057504a67d32536062142b2e155ce9
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 14:17:38 +0300

    Добавил решение Task66

--- Lesson_09_Task66/Program.cs
+++ Lesson_09_Task66/Program.cs
@@ -3,3 +3,20 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberN > 0)
+{
+    Console.Write("M = " + numberM + "; N = " + numberN + " -> ");
+    Console.WriteLine(NaturalNumbersRangeSum(numberM, numberN));
+}
+else Console.WriteLine("Число должно быть больше 0");
+
+int NaturalNumbersRangeSum(int numM, int numN)
+{
+    if (numM == numN) return numM;
+    else return numN + NaturalNumbersRangeSum(numM, numN - 1);
+}
