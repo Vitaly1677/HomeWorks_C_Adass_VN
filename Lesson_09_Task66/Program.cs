beef6be531687bdb7a1292c4591136dfc21e82f6
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 14:01:32 +0300

    Добавил задание

--- Lesson_09_Task66/Program.cs
+++ Lesson_09_Task66/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
