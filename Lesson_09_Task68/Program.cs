5feabf1ac2594e6ae8b290be8e98a1473ea31dda
Vitaly Adass <geekpython16@yandex.ru>
2023-02-06 14:20:00 +0300

    Добавил решение Task66

--- Lesson_09_Task68/Program.cs
+++ Lesson_09_Task68/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
